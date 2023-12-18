2df80746eb6c2e0ee0c4c613d737fa572fe76d3e
axha <axha@averna.com>
2023-12-18 11:39:58 -0500

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
+import { BASE_CONFIG, SITE_CONFIG } from "./src/utils/config-utils.ts";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://axhatem.github.io',
-  base: '/plumbertest',
+  site: SITE_CONFIG,
+  base: BASE_CONFIG,
 });
